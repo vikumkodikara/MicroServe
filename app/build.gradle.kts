965c28e15b36542a59d0de6b1136dfc900440aef
vikumkodikara <vikumkodikara123@gmail.com>
2026-05-25 19:16:35 +0530

    Add Firebase auth, Firestore, and manifest setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 }
