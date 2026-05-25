6662a90dd40fceb5cb6cbea23e1e3b82c5424326
vikumkodikara <vikumkodikara123@gmail.com>
2026-05-25 22:52:55 +0530

    Update app initialization

--- app/src/main/java/com/example/microserve/MicroServeApp.kt
+++ app/src/main/java/com/example/microserve/MicroServeApp.kt
@@ -2,12 +2,24 @@ package com.example.microserve
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MicroServeApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
+        enableFirestoreOfflineCache()
         applySavedTheme()
+    }
+
+    private fun enableFirestoreOfflineCache() {
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+        FirebaseFirestore.getInstance().firestoreSettings = settings
     }
 
     private fun applySavedTheme() {
